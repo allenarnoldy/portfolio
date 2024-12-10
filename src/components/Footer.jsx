6839bfe1f818bb571a37904fc372a3d604395a2b
allenarnoldy <allenarnoldy@gmail.com>
2024-12-09 18:16:12 -0600

    Adding more code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,28 @@
 export default function Footer() {
-    return (
-        <>
-        <footer>
-            <section>
-                <b> Allen Arnoldy all right reserved</b>
-            </section>
-        </footer>
-        </>
-    )
+  return (
+    <>
+      <footer>
+        <section>
+          <b> © 2024 All right reserved</b>
+        </section>
+        <section>
+          <a
+            href="https://github.com/allenarnoldy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          |
+          <a
+            href="https://www.linkedin.com/in/allen-arnoldy-cs/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}LinkedIn
+          </a>
+        </section>
+      </footer>
+    </>
+  );
 }
